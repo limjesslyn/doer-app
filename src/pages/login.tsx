f59c90108b01e35fcbbcc442533813c33cce7ec5
limjesslyn <jesslynlim.0252@gmail.com>
2023-11-10 01:57:49 +0700

    add: app name to login and reg page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -55,7 +55,11 @@ export default function login() {
             <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Login to your account
+                        Login to your 
+                        {' '}
+                        <span className='underline'>Doer</span>
+                        {' '} 
+                        account
                     </h2>
                 </div>
 
